3436c53daa6f9bb690b8420fde291d4115f35b46
jk2937 <kaschakjonathan+github@gmail.com>
2023-12-01 23:56:22 -0500

    Added api code

--- gen-ai-server/api.py
+++ gen-ai-server/api.py
@@ -1,27 +1,25 @@
 import requests
+import datetime
 
 API_URL = "https://api-inference.huggingface.co/models/Meina/MeinaMix_V10"
-API_TOKEN = open("huggingface_access_token.txt", "r").read()
+API_TOKEN = open("huggingface_access_token.txt", "r").read().rstrip("\n")
 
 print(API_TOKEN)
 
 headers = {"Authorization": f"Bearer {API_TOKEN}"}
 
-print(headers)
-
-
-quit()
-
-print("foobar")
-
-
 def query(payload):
 	response = requests.post(API_URL, headers=headers, json=payload)
 	return response.content
+
 image_bytes = query({
 	"inputs": "Astronaut riding a horse",
 })
+
+print(str(image_bytes))
 # You can access the image with PIL.Image for example
 import io
 from PIL import Image
 image = Image.open(io.BytesIO(image_bytes))
+image_path = "./" + str(datetime.datetime.now().strftime("%H%M%S%f")) + ".png"
+image.save("./{image_path}.png")
--- gen-ai-server/database.py
+++ gen-ai-server/database.py
@@ -1,16 +1,11 @@
 import sqlite3
 
 class database:
-
     def __init__(self, dbname="database.db"):
-
         self.connection = sqlite3.connect(dbname)
 
-
     def create_table(self):
-
         cursor = self.connection.cursor()
-
         cursor.execute(
             """
             CREATE TABLE IF NOT EXISTS fish_inventory (
@@ -21,16 +16,24 @@ class database:
             )
             """
         )
-
         
         self.connection.commit()
         cursor.close()
 
+    def create_image_table(self):
+        cursor = self.connection.cursor()
+        cursor.execute(
+            """
+            CREATE TABLE image_table (
+                image BLOB,
+                date_created DATE,
+                session_id VARCHAR(50)
+            );
+            """
+        )
 
     def insert_data(self):
-
         cursor = self.connection.cursor()
-
         cursor.execute(
             """
             INSERT INTO fish_inventory (name, species, quantity)
@@ -39,51 +42,46 @@ class database:
             ("Nemo", "Clownfish", 10)
         )
 
-
         self.connection.commit()
         cursor.close()
 
+    def insert_image_data(image, date_created, session_id):
+        cursor = self.connection.cursor()
+        cursor.execute(
+            """
+            INSERT INTO image_table (image, date_created, session_id)
+            VALUES (?, ?, ?);
+            """,
+            (image, date_created, session_id)
+        )
 
     def read_data(self):
-
         cursor = self.connection.cursor()
-
         cursor.execute("SELECT * FROM fish_inventory")
         fish_data = cursor.fetchall()
-
-        for fish in fish_data:
             print(f"Fish ID: {fish[0]}, Name: {fish[1]}, Species: {fish[2]}, Quantity: {fish[3]}")
-
 
         cursor.close()
 
 
-    def deinit(self):
 
+    def deinit(self):
         self.connection.close()
 
-
     def test(self):
-
         try:
             self.__init__()
         except ProgrammingError:
             print("Error")
-
         self.create_table()
         self.insert_data()
         self.read_data()
 
-
         self.deinit()
 
-
 def main():
-
     db = database()
     db.test()
 
-
 if __name__ == "__main__":
-    
     main()
